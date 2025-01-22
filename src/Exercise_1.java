60856212c87ed03aa7d5a4a5008d3d0e3c5ef6ba
Acer <kishanmpatil@gmail.com>
2025-01-22 23:08:41 +0530

    commit exercise_1

---
+++ src/Exercise_1.java
@@ -0,0 +1,8 @@
+public class Exercise_1 {
+    public static void main(String[] args){
+        int sum = 24 + 26;
+        System.out.println(sum);
+    }
+
+
+}
---
+++ src/ex_04_Literals.java
@@ -0,0 +1,13 @@
+public class ex_04_Literals {
+
+    public static void main(String[] args) {
+//        //Literal
+//        int age=65;
+//        age=66;
+//        // connstant
+//        final int girl_age =18;
+//        //girl_age=19;
+        System.out.println("HI KISHAN");
+
+    }
+}
